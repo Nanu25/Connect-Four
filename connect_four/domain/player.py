df31a734698ab479ee325b7aba31cfbcfce61e25
Grancea Alexandru <103886681+Nanu25@users.noreply.github.com>
2024-09-18 15:27:52 +0300

    Add files via upload

---
+++ connect_four/domain/player.py
@@ -0,0 +1,14 @@
+class Player():
+    def __init__(self, name, symbol):
+        self.__name = name
+        self.__symbol = symbol
+    @property
+    def name(self):
+        return self.__name
+
+    @property
+    def symbol(self):
+        return self.__symbol
+
+    def __str__(self):
+        return "Player name is: " + self.__name + " and plays with symbol: " + self.__symbol
